4db3abca0749e47d725c1408530702292fce3c6e
Kamil Matysiewicz <kamil@aexol.com>
2018-02-12 09:59:42 +0100

    Build.

--- lib/middleware.d.ts
+++ lib/middleware.d.ts
@@ -1,10 +1,10 @@
-import { IResponse } from './types/response';
-export { IResponse } from './types/response';
+import { IResponse, IResponsePayload, IResponseStatus } from './types/response';
+export { IResponse, IResponsePayload, IResponseStatus } from './types/response';
 export interface ISyncanoContext {
     args: object;
     meta: object;
     config: object;
 }
-export declare type IHandler = (ctx: ISyncanoContext, syncano: object) => Promise<IResponse>;
+export declare type IHandler = (ctx: ISyncanoContext, syncano: object) => Promise<IResponse | IResponsePayload | IResponseStatus>;
 declare function serve(ctx: ISyncanoContext, handler: IHandler): Promise<object>;
 export default serve;
