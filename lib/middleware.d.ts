1c54cf805cb8de8d56877f542bd7366377a03369
Kamil Matysiewicz <kamil@aexol.com>
2018-02-14 15:57:13 +0100

    Syncano context refined.

--- lib/middleware.d.ts
+++ lib/middleware.d.ts
@@ -1,13 +1,37 @@
 import { IResponse, IResponsePayload, IResponseStatus } from './types/response';
 export { IResponse, IResponsePayload, IResponseStatus } from './types/response';
+export interface ISyncanoRequestArgs {
+    [s: string]: any;
+}
+export interface ISyncanoRequestConfig {
+    [s: string]: any;
+}
+export interface ISyncanoRequestMetaRequest {
+    [s: string]: string;
+}
+export interface ISyncanoRequestMetaMetadataParameters {
+    [s: string]: object;
+}
+export interface ISyncanoRequestMetaMetadata {
+    description?: string;
+    parameters?: ISyncanoRequestMetaMetadataParameters;
+}
+export interface ISyncanoRequestMeta {
+    socket: string;
+    request: ISyncanoRequestMetaRequest;
+    instance: string;
+    token: string;
+    executor: string;
+    executed_by: string;
+    api_host: string;
+    space_host: string;
+    metadata: ISyncanoRequestMetaMetadataParameters;
+}
 export interface ISyncanoContext {
-    args: object;
-    meta: object;
-    config: object;
+    args: ISyncanoRequestArgs;
+    meta: ISyncanoRequestMeta;
+    config: ISyncanoRequestConfig;
 }
 export declare type HandlerFn = (ctx: ISyncanoContext, syncano: object) => Promise<IResponse | IResponsePayload | IResponseStatus>;
-export interface IHandler {
-    handle: HandlerFn;
-}
 declare function serve(ctx: ISyncanoContext, handler: HandlerFn): Promise<object>;
 export default serve;
--- src/middleware.ts
+++ src/middleware.ts
@@ -8,10 +8,42 @@ import {IResponse,
   isIResponseStatus} from './types/response';
 export {IResponse, IResponsePayload, IResponseStatus} from './types/response';
 
+export interface ISyncanoRequestArgs  {
+  [s: string]: any;
+}
+
+export interface ISyncanoRequestConfig  {
+  [s: string]: any;
+}
+
+export interface ISyncanoRequestMetaRequest  {
+  [s: string]: string;
+}
+
+export interface ISyncanoRequestMetaMetadataParameters  {
+  [s: string]: object;
+}
+export interface ISyncanoRequestMetaMetadata  {
+  description?: string;
+  parameters?: ISyncanoRequestMetaMetadataParameters;
+}
+
+export interface ISyncanoRequestMeta  {
+  socket: string;
+  request: ISyncanoRequestMetaRequest;
+  instance: string;
+  token: string;
+  executor: string;
+  executed_by: string;
+  api_host: string;
+  space_host: string;
+  metadata: ISyncanoRequestMetaMetadataParameters;
+}
+
 export interface ISyncanoContext {
-  args: object;
-  meta: object;
-  config: object;
+  args: ISyncanoRequestArgs;
+  meta: ISyncanoRequestMeta;
+  config: ISyncanoRequestConfig;
 }
 
 function isISyncanoContext(o: object): o is ISyncanoContext {
