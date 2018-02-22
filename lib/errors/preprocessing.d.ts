c5a6f35b3d5cce074cfa34e67187acd69cf6b104
Kamil Matysiewicz <kamil@aexol.com>
2018-02-22 14:26:25 +0100

    Remove lib from repo.

--- lib/errors/preprocessing.d.ts
+++
@@ -1,7 +0,0 @@
-import { IResponse } from '../types/response';
-export declare class PreprocessingError extends Error implements IResponse {
-    payload: object;
-    status: number;
-    constructor(details: object);
-    merge(newResponse: IResponse): IResponse;
-}
